31a7a3a449b0c7adcee0ebd8cdfd6a24887fa77f
Benjamin Stürz <benni@stuerz.xyz>
2024-05-20 21:51:47 +0000

    add missing src/decorder.rs

---
+++ src/decoder.rs
@@ -0,0 +1,48 @@
+use std::io::{BufReader, Error, ErrorKind, Read, Result, Seek, SeekFrom};
+
+use bincode::{config::{Configuration, Fixint, LittleEndian, NoLimit}, Decode};
+
+
+pub struct Decoder<T> {
+	inner: BufReader<T>,
+	config: Configuration<LittleEndian, Fixint, NoLimit>,
+}
+
+impl<T: Read> Decoder<T> {
+	pub fn new(inner: T) -> Self {
+		Self {
+			inner: BufReader::new(inner),
+			config: bincode::config::standard()
+				.with_fixed_int_encoding()
+				.with_little_endian(),
+		}
+	}
+
+	pub fn decode<X: Decode>(&mut self) -> Result<X> {
+		bincode::decode_from_reader(&mut self.inner, self.config)
+			.map_err(|_| Error::new(ErrorKind::InvalidInput, "failed to decode"))
+	}
+
+	pub fn read(&mut self, buf: &mut [u8]) -> Result<()> {
+		self.inner.read_exact(buf)
+	}
+
+	pub fn get_ref(&self) -> &T {
+		self.inner.get_ref()
+	}
+
+}
+
+impl<T: Read + Seek> Decoder<T> {
+	pub fn decode_at<X: Decode>(&mut self, pos: u64) -> Result<X> {
+		self.seek(pos)?;
+		self.decode()
+	}
+	pub fn seek(&mut self, pos: u64) -> Result<()> {
+		self.inner.seek(SeekFrom::Start(pos))?;
+		Ok(())
+	}
+	pub fn seek_relative(&mut self, off: i64) -> Result<()> {
+		self.inner.seek_relative(off)
+	}
+}
