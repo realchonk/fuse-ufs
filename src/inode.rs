43e13781d1713e1fb5b8646eba702cbeed497eb3
Benjamin Stürz <benni@stuerz.xyz>
2024-05-20 18:26:07 +0000

    fmt
    hard tabs + tabsize=4

--- src/inode.rs
+++ src/inode.rs
@@ -1,7 +1,7 @@
 use fuser::{FileAttr, FileType};
 use std::{
-    mem::size_of,
-    time::{Duration, SystemTime},
+	mem::size_of,
+	time::{Duration, SystemTime},
 };
 
 use crate::{UfsDaddr, UfsTime};
@@ -23,45 +23,45 @@ pub const UFS_NIADDR: usize = 3;
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub struct UfsInodeBlocks {
-    pub direct: [UfsDaddr; UFS_NDADDR],
-    pub indirect: [UfsDaddr; UFS_NIADDR],
+	pub direct: [UfsDaddr; UFS_NDADDR],
+	pub indirect: [UfsDaddr; UFS_NIADDR],
 }
 
 #[allow(dead_code)]
 #[repr(C)]
 pub union UfsInodeData {
-    pub blocks: UfsInodeBlocks,
-    pub shortlink: [u8; (UFS_NDADDR + UFS_NIADDR) * size_of::<UfsDaddr>()],
+	pub blocks: UfsInodeBlocks,
+	pub shortlink: [u8; (UFS_NDADDR + UFS_NIADDR) * size_of::<UfsDaddr>()],
 }
 
 #[allow(dead_code)]
 #[repr(C)]
 pub struct Inode {
-    pub mode: u16,                    //   0: IFMT, permissions; see below.
-    pub nlink: u16,                   //   2: File link count.
-    pub uid: u32,                     //   4: File owner.
-    pub gid: u32,                     //   8: File group.
-    pub blksize: u32,                 //  12: Inode blocksize.
-    pub size: u64,                    //  16: File byte count.
-    pub blocks: u64,                  //  24: Blocks actually held.
-    pub atime: UfsTime,               //  32: Last access time.
-    pub mtime: UfsTime,               //  40: Last modified time.
-    pub ctime: UfsTime,               //  48: Last inode change time.
-    pub birthtime: UfsTime,           //  56: Inode creation time.
-    pub mtimensec: u32,               //  64: Last modified time.
-    pub atimensec: u32,               //  68: Last access time.
-    pub ctimensec: u32,               //  72: Last inode change time.
-    pub birthnsec: u32,               //  76: Inode creation time.
-    pub gen: u32,                     //  80: Generation number.
-    pub kernflags: u32,               //  84: Kernel flags.
-    pub flags: u32,                   //  88: Status flags (chflags).
-    pub extsize: u32,                 //  92: External attributes size.
-    pub extb: [UfsDaddr; UFS_NXADDR], //  96: External attributes block.
-    pub data: UfsInodeData,           // XXX: Blocks
-    pub modrev: u64,                  // 232: i_modrev for NFSv4
-    pub ignored: u32, // 240: (SUJ: Next unlinked inode) or (IFDIR: depth from root dir)
-    pub ckhash: u32,  // 244: if CK_INODE, its check-hash
-    pub spare: [u32; 2], // 248: Reserved; currently unused
+	pub mode: u16,                    //   0: IFMT, permissions; see below.
+	pub nlink: u16,                   //   2: File link count.
+	pub uid: u32,                     //   4: File owner.
+	pub gid: u32,                     //   8: File group.
+	pub blksize: u32,                 //  12: Inode blocksize.
+	pub size: u64,                    //  16: File byte count.
+	pub blocks: u64,                  //  24: Blocks actually held.
+	pub atime: UfsTime,               //  32: Last access time.
+	pub mtime: UfsTime,               //  40: Last modified time.
+	pub ctime: UfsTime,               //  48: Last inode change time.
+	pub birthtime: UfsTime,           //  56: Inode creation time.
+	pub mtimensec: u32,               //  64: Last modified time.
+	pub atimensec: u32,               //  68: Last access time.
+	pub ctimensec: u32,               //  72: Last inode change time.
+	pub birthnsec: u32,               //  76: Inode creation time.
+	pub gen: u32,                     //  80: Generation number.
+	pub kernflags: u32,               //  84: Kernel flags.
+	pub flags: u32,                   //  88: Status flags (chflags).
+	pub extsize: u32,                 //  92: External attributes size.
+	pub extb: [UfsDaddr; UFS_NXADDR], //  96: External attributes block.
+	pub data: UfsInodeData,           // XXX: Blocks
+	pub modrev: u64,                  // 232: i_modrev for NFSv4
+	pub ignored: u32,                 // 240: (SUJ: Next unlinked inode) or (IFDIR: depth from root dir)
+	pub ckhash: u32,                  // 244: if CK_INODE, its check-hash
+	pub spare: [u32; 2],              // 248: Reserved; currently unused
 }
 
 /// type of file mask
@@ -82,67 +82,67 @@ const S_IFLNK: u16 = 0o120000;
 const S_IFSOCK: u16 = 0o140000;
 
 fn timetosys(mut s: UfsTime, ns: u32) -> SystemTime {
-    let neg = s < 0;
-    if neg {
-        s = -s;
-    }
-    let dur = Duration::new(s as u64, ns);
-    let mut time = SystemTime::UNIX_EPOCH;
-    if neg {
-        time -= dur;
-    } else {
-        time += dur;
-    }
-    time
+	let neg = s < 0;
+	if neg {
+		s = -s;
+	}
+	let dur = Duration::new(s as u64, ns);
+	let mut time = SystemTime::UNIX_EPOCH;
+	if neg {
+		time -= dur;
+	} else {
+		time += dur;
+	}
+	time
 }
 
 impl Inode {
-    pub fn atime(&self) -> SystemTime {
-        timetosys(self.atime, self.atimensec)
-    }
-    pub fn mtime(&self) -> SystemTime {
-        timetosys(self.mtime, self.mtimensec)
-    }
-    pub fn ctime(&self) -> SystemTime {
-        timetosys(self.ctime, self.ctimensec)
-    }
-    pub fn btime(&self) -> SystemTime {
-        timetosys(self.birthtime, self.birthnsec)
-    }
-    pub fn perm(&self) -> u16 {
-        self.mode & 0o7777
-    }
-    pub fn kind(&self) -> FileType {
-        let mode = self.mode & S_IFMT;
-        match mode {
-            S_IFIFO => FileType::NamedPipe,
-            S_IFCHR => FileType::CharDevice,
-            S_IFDIR => FileType::Directory,
-            S_IFBLK => FileType::BlockDevice,
-            S_IFREG => FileType::RegularFile,
-            S_IFLNK => FileType::Symlink,
-            S_IFSOCK => FileType::Socket,
-            _ => unreachable!("invalid file mode: {mode:o}"),
-        }
-    }
+	pub fn atime(&self) -> SystemTime {
+		timetosys(self.atime, self.atimensec)
+	}
+	pub fn mtime(&self) -> SystemTime {
+		timetosys(self.mtime, self.mtimensec)
+	}
+	pub fn ctime(&self) -> SystemTime {
+		timetosys(self.ctime, self.ctimensec)
+	}
+	pub fn btime(&self) -> SystemTime {
+		timetosys(self.birthtime, self.birthnsec)
+	}
+	pub fn perm(&self) -> u16 {
+		self.mode & 0o7777
+	}
+	pub fn kind(&self) -> FileType {
+		let mode = self.mode & S_IFMT;
+		match mode {
+			S_IFIFO => FileType::NamedPipe,
+			S_IFCHR => FileType::CharDevice,
+			S_IFDIR => FileType::Directory,
+			S_IFBLK => FileType::BlockDevice,
+			S_IFREG => FileType::RegularFile,
+			S_IFLNK => FileType::Symlink,
+			S_IFSOCK => FileType::Socket,
+			_ => unreachable!("invalid file mode: {mode:o}"),
+		}
+	}
 
-    pub fn as_fileattr(&self, ino: u64) -> FileAttr {
-        FileAttr {
-            ino,
-            size: self.size,
-            blocks: self.blocks,
-            atime: self.atime(),
-            mtime: self.mtime(),
-            ctime: self.ctime(),
-            crtime: self.btime(),
-            kind: self.kind(),
-            perm: self.perm(),
-            nlink: self.nlink.into(),
-            uid: self.uid,
-            gid: self.gid,
-            rdev: 0,
-            blksize: self.blksize,
-            flags: self.flags,
-        }
-    }
+	pub fn as_fileattr(&self, ino: u64) -> FileAttr {
+		FileAttr {
+			ino,
+			size: self.size,
+			blocks: self.blocks,
+			atime: self.atime(),
+			mtime: self.mtime(),
+			ctime: self.ctime(),
+			crtime: self.btime(),
+			kind: self.kind(),
+			perm: self.perm(),
+			nlink: self.nlink.into(),
+			uid: self.uid,
+			gid: self.gid,
+			rdev: 0,
+			blksize: self.blksize,
+			flags: self.flags,
+		}
+	}
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,11 @@
 use std::{
-    ffi::OsString,
-    fmt, fs,
-    os::unix::ffi::OsStringExt,
-    path::PathBuf,
-    process::{Child, Command},
-    thread::sleep,
-    time::{Duration, Instant},
+	ffi::OsString,
+	fmt, fs,
+	os::unix::ffi::OsStringExt,
+	path::PathBuf,
+	process::{Child, Command},
+	thread::sleep,
+	time::{Duration, Instant},
 };
 
 use assert_cmd::cargo::CommandCargoExt;
@@ -15,42 +15,42 @@ use rstest::{fixture, rstest};
 use tempfile::{tempdir, TempDir};
 
 fn prepare_image(filename: &str) -> PathBuf {
-    let mut zimg = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    zimg.push("resources");
-    zimg.push(filename);
-    zimg.set_extension("img.zst");
-    let mut img = PathBuf::from(env!("CARGO_TARGET_TMPDIR"));
-    img.push(filename);
+	let mut zimg = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+	zimg.push("resources");
+	zimg.push(filename);
+	zimg.set_extension("img.zst");
+	let mut img = PathBuf::from(env!("CARGO_TARGET_TMPDIR"));
+	img.push(filename);
 
-    // If the golden image doesn't exist, or is out of date, rebuild it
-    // Note: we can't accurately compare the two timestamps with less than 1
-    // second granularity due to a zstd bug.
-    // https://github.com/facebook/zstd/issues/3748
-    let zmtime = fs::metadata(&zimg).unwrap().modified().unwrap();
-    let mtime = fs::metadata(&img);
-    if mtime.is_err() || (mtime.unwrap().modified().unwrap() + Duration::from_secs(1)) < zmtime {
-        Command::new("unzstd")
-            .arg("-f")
-            .arg("-o")
-            .arg(&img)
-            .arg(&zimg)
-            .output()
-            .expect("Uncompressing golden image failed");
-    }
-    img
+	// If the golden image doesn't exist, or is out of date, rebuild it
+	// Note: we can't accurately compare the two timestamps with less than 1
+	// second granularity due to a zstd bug.
+	// https://github.com/facebook/zstd/issues/3748
+	let zmtime = fs::metadata(&zimg).unwrap().modified().unwrap();
+	let mtime = fs::metadata(&img);
+	if mtime.is_err() || (mtime.unwrap().modified().unwrap() + Duration::from_secs(1)) < zmtime {
+		Command::new("unzstd")
+			.arg("-f")
+			.arg("-o")
+			.arg(&img)
+			.arg(&zimg)
+			.output()
+			.expect("Uncompressing golden image failed");
+	}
+	img
 }
 
 lazy_static! {
-    pub static ref GOLDEN: PathBuf = prepare_image("ufs.img");
+	pub static ref GOLDEN: PathBuf = prepare_image("ufs.img");
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct WaitForError;
 
 impl fmt::Display for WaitForError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "timeout waiting for condition")
-    }
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "timeout waiting for condition")
+	}
 }
 
 impl std::error::Error for WaitForError {}
@@ -58,91 +58,91 @@ impl std::error::Error for WaitForError {}
 /// Wait for a limited amount of time for the given condition to be true.
 pub fn waitfor<C>(timeout: Duration, condition: C) -> Result<(), WaitForError>
 where
-    C: Fn() -> bool,
+	C: Fn() -> bool,
 {
-    let start = Instant::now();
-    loop {
-        if condition() {
-            break Ok(());
-        }
-        if start.elapsed() > timeout {
-            break (Err(WaitForError));
-        }
-        sleep(Duration::from_millis(50));
-    }
+	let start = Instant::now();
+	loop {
+		if condition() {
+			break Ok(());
+		}
+		if start.elapsed() > timeout {
+			break (Err(WaitForError));
+		}
+		sleep(Duration::from_millis(50));
+	}
 }
 
 struct Harness {
-    d: TempDir,
-    child: Child,
+	d: TempDir,
+	child: Child,
 }
 
 #[fixture]
 fn harness() -> Harness {
-    let d = tempdir().unwrap();
-    let child = Command::cargo_bin("fuse-ufs")
-        .unwrap()
-        .arg(GOLDEN.as_path())
-        .arg(d.path())
-        .spawn()
-        .unwrap();
+	let d = tempdir().unwrap();
+	let child = Command::cargo_bin("fuse-ufs")
+		.unwrap()
+		.arg(GOLDEN.as_path())
+		.arg(d.path())
+		.spawn()
+		.unwrap();
 
-    waitfor(Duration::from_secs(5), || {
-        let s = nix::sys::statfs::statfs(d.path()).unwrap();
-        cfg_if! {
-            if #[cfg(target_os = "freebsd")] {
-                s.filesystem_type_name() == "fusefs"
-            } else if #[cfg(target_os = "linux")] {
-                s.filesystem_type() == nix::sys::statfs::FUSE_SUPER_MAGIC
-            }
-        }
-    })
-    .unwrap();
+	waitfor(Duration::from_secs(5), || {
+		let s = nix::sys::statfs::statfs(d.path()).unwrap();
+		cfg_if! {
+			if #[cfg(target_os = "freebsd")] {
+				s.filesystem_type_name() == "fusefs"
+			} else if #[cfg(target_os = "linux")] {
+				s.filesystem_type() == nix::sys::statfs::FUSE_SUPER_MAGIC
+			}
+		}
+	})
+	.unwrap();
 
-    Harness { d, child }
+	Harness { d, child }
 }
 
 impl Drop for Harness {
-    #[allow(clippy::if_same_then_else)]
-    fn drop(&mut self) {
-        loop {
-            let cmd = Command::new("umount").arg(self.d.path()).output();
-            match cmd {
-                Err(e) => {
-                    eprintln!("Executing umount failed: {}", e);
-                    if std::thread::panicking() {
-                        // Can't double panic
-                        return;
-                    }
-                    panic!("Executing umount failed");
-                }
-                Ok(output) => {
-                    let errmsg = OsString::from_vec(output.stderr).into_string().unwrap();
-                    if output.status.success() {
-                        break;
-                    } else if errmsg.contains("not a file system root directory") {
-                        // The daemon probably crashed.
-                        break;
-                    } else if errmsg.contains("Device busy") {
-                        println!("{}", errmsg);
-                    } else {
-                        if std::thread::panicking() {
-                            // Can't double panic
-                            println!("{}", errmsg);
-                            return;
-                        }
-                        panic!("{}", errmsg);
-                    }
-                }
-            }
-            sleep(Duration::from_millis(50));
-        }
-        let _ = self.child.wait();
-    }
+	#[allow(clippy::if_same_then_else)]
+	fn drop(&mut self) {
+		loop {
+			let cmd = Command::new("umount").arg(self.d.path()).output();
+			match cmd {
+				Err(e) => {
+					eprintln!("Executing umount failed: {}", e);
+					if std::thread::panicking() {
+						// Can't double panic
+						return;
+					}
+					panic!("Executing umount failed");
+				}
+				Ok(output) => {
+					let errmsg = OsString::from_vec(output.stderr).into_string().unwrap();
+					if output.status.success() {
+						break;
+					} else if errmsg.contains("not a file system root directory") {
+						// The daemon probably crashed.
+						break;
+					} else if errmsg.contains("Device busy") {
+						println!("{}", errmsg);
+					} else {
+						if std::thread::panicking() {
+							// Can't double panic
+							println!("{}", errmsg);
+							return;
+						}
+						panic!("{}", errmsg);
+					}
+				}
+			}
+			sleep(Duration::from_millis(50));
+		}
+		let _ = self.child.wait();
+	}
 }
 
 /// Mount and unmount the golden image
 #[rstest]
 fn mount(harness: Harness) {
-    drop(harness);
+	drop(harness);
 }
