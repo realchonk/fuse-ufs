9bc0025b72eac11dfc588865e633c50b44dbc990
Benjamin Stürz <benni@stuerz.xyz>
2024-08-19 22:03:31 +0200

    remove broken -v option for now (#58)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use clap::Parser;
-use log::LevelFilter;
 
 use crate::{cli::Cli, ufs::Ufs};
 
@@ -14,10 +13,6 @@ mod ufs;
 fn main() -> Result<()> {
 	env_logger::init();
 	let cli = Cli::parse();
-
-	if cli.verbose {
-		log::set_max_level(LevelFilter::Trace);
-	}
 
 	let fs = Ufs::open(&cli.device)?;
 
