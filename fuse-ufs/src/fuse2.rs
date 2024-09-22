d01bfe539de7901ce737cfcb1ff63ae34e62e553
Benjamin Stürz <benni@stuerz.xyz>
2024-09-22 16:57:17 +0000

    fuse-ufs/fuse2: implement basic Filesystem functions

--- fuse-ufs/src/fuse2.rs
+++ fuse-ufs/src/fuse2.rs
@@ -1,7 +1,22 @@
-use std::{io::{Error, Result}, path::Path};
+use std::{ffi::CString, io::{Error, Result}, os::unix::ffi::OsStrExt, path::Path};
 use fuse2rs::*;
+use rufs::InodeNum;
 
 use crate::Fs;
+
+impl Fs {
+	fn lookup(&mut self, path: &Path) -> Result<InodeNum> {
+		if !path.is_absolute() {
+			return Err(Error::from_raw_os_error(libc::EINVAL));
+		}
+
+		let mut inr = InodeNum::ROOT;
+		for comp in path.components().skip(1) {
+			inr = self.ufs.dir_lookup(inr, comp.as_os_str())?;
+		}
+		Ok(inr)
+	}
+}
 
 impl Filesystem for Fs {
 	fn getattr(
@@ -9,7 +24,9 @@ impl Filesystem for Fs {
 		_req: &Request,
 		path: &Path,
 	) -> Result<FileAttr> {
-		Err(Error::from_raw_os_error(libc::ENOSYS))
+		let inr = self.lookup(path)?;
+		let ino = self.ufs.inode_attr(inr)?;
+		Ok(ino.into())
 	}
 
 	fn readdir(
@@ -20,6 +37,31 @@ impl Filesystem for Fs {
 		filler: &mut DirFiller,
 		_info: &FileInfo,
 	) -> Result<()> {
+		let pinr = self.lookup(path)?;
+
+		// TODO
+		if off != 0 {
+			return Ok(());
+		}
+
+		let mut i = 0;
+
+		self.ufs.dir_iter(pinr, |name, _inr, _kind| {
+			i += 1;
+
+			if i <= off {
+				return None;
+			}
+
+			let name = CString::new(name.as_bytes().to_vec()).unwrap();
+
+			if filler.push(&name) {
+				None
+			} else {
+				Some(())
+			}
+		})?;
+		
 		Err(Error::from_raw_os_error(libc::ENOSYS))
 	}
 
@@ -31,7 +73,9 @@ impl Filesystem for Fs {
 		buf: &mut [u8],
 		_info: &FileInfo,
 	) -> Result<usize> {
-		Err(Error::from_raw_os_error(libc::ENOSYS))
+		let inr = self.lookup(path)?;
+		let num = self.ufs.inode_read(inr, off, buf)?;
+		Ok(num)
 	}
 }
 
