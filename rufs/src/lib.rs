b07ac172d0a2b0297972a7e3e6bdb709bd0a0524
Benjamin Stürz <benni@stuerz.xyz>
2025-05-15 22:03:02 +0000

    rufs: make caching optional

--- rufs/src/lib.rs
+++ rufs/src/lib.rs
@@ -7,18 +7,26 @@ mod inode;
 mod ufs;
 
 /// Number of inodes to store in a cache.
+#[cfg(feature = "icache")]
 const ICACHE_SIZE: usize = 1024;
 
 /// Number of blocks cached.
+#[cfg(feature = "bcache")]
 const BCACHE_SIZE: usize = 16;
 
 /// Number of directory entries to cache.
+#[cfg(feature = "dcache")]
 const DCACHE_SIZE: usize = 1024;
 
 #[cfg(any(target_os = "freebsd", target_os = "openbsd", target_os = "macos"))]
 pub const ENOATTR: i32 = libc::ENOATTR;
 #[cfg(target_os = "linux")]
 pub const ENOATTR: i32 = libc::ENODATA;
+
+#[cfg(feature = "lru")]
+fn new_lru<K: std::hash::Hash + Eq, V>(size: usize) -> lru::LruCache<K, V> {
+	lru::LruCache::new(std::num::NonZeroUsize::new(size).unwrap())
+}
 
 pub use crate::{
 	blockreader::{Backend, BlockReader},
